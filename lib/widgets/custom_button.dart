744b3dd46d06e8a34d54c87d403adcb4e3812ca0
enisodesen <e.odesen@gmail.com>
2023-01-17 16:18:58 +0300

    users list in another page

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, this.onPressed, required this.text});
+
+  final void Function()? onPressed;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onPressed, child: Text(text));
+  }
+}
